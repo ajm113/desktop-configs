60a48be23959a2637942e32dc7cf4a3f764bd09d
Andrew McRobb <amcrobb@networthservices.com>
2018-09-26 18:47:49 +0000

    Updated config to match work desktop.

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,5 @@
+alias ls='ls --color=auto'
+alias werk='cd ~/Projects'
+alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
+alias start='npm start'
 neofetch
