29a6b64b0fdd0e3b72e0f0eb7bc5e5f7add93f5e
Andrew McRobb <amcrobb@networthservices.com>
2019-08-27 15:18:34 -0700

    Updated configs to match latest changes.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 alias ls='ls --color=auto'
+alias bc='bc -l'
 alias werk='cd ~/Projects'
 alias grep='grep -s --exclude-dir=node_modules --exclude-dir=.git --exclude=bundle.js --exclude=package-lock.json --color=always'
 alias start='npm start'
-neofetch
+neofetch --ascii ~/.config/fish/cage.txt --ascii_colors 1 2 3 4 5 6 --cpu_display
+
+set -x GOPATH /home/andrew/go
+
+set -x PATH $PATH $GOPATH/bin
